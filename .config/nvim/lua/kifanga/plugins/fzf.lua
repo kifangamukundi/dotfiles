b15ce5c0d73a9e3ee19e4819c1d88d081a6b2faf
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-08-13 11:55:25 -0400

    fzf-lua: added fuzzy finder

---
+++ .config/nvim/lua/kifanga/plugins/fzf.lua
@@ -0,0 +1,59 @@
+return {
+    "ibhagwan/fzf-lua",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        local fzf = require("fzf-lua")
+
+        fzf.setup({
+            winopts = {
+                height = 0.85,
+                width = 0.80,
+                row = 0.35,
+                col = 0.50,
+                border = "rounded",
+            },
+            previewer = {
+                layout = "vertical",
+                vertical = "up:45%",
+            },
+            files = {
+                prompt = "Files❯ ",
+                fd_opts = "--color=never --type f --hidden --follow --exclude .git",
+            },
+            live_grep = {
+                prompt = "Live Grep❯ ",
+                rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case",
+                file_opts = "--iglob '!{.git,node_modules}/*'",
+            },
+            buffers = { prompt = "Buffers❯ " },
+            help_tags = { prompt = "Help❯ " },
+            marks = { prompt = "Marks❯ " },
+            keymap = {
+                fzf = {
+                    ["ctrl-q"] = "select-all+accept",
+                },
+                builtin = {
+                    ["<C-u>"] = "preview-page-up",
+                    ["<C-d>"] = "preview-page-down",
+                },
+            },
+        })
+
+        local map = vim.keymap.set
+
+        map("n", "<leader>ff", fzf.files, { desc = "Find Files" })
+        map("n", "<leader>fg", fzf.git_files, { desc = "Find Git Files" })
+        map("n", "<leader>fl", fzf.live_grep, { desc = "Live Grep" })
+        map("n", "<leader>fb", fzf.buffers, { desc = "Find Buffers" })
+        map("n", "<leader>fd", fzf.diagnostics_document, { desc = "Document Diagnostics" })
+        map("n", "<leader>fh", fzf.help_tags, { desc = "Help Tags" })
+        map("n", "<leader>fm", fzf.marks, { desc = "Marks" })
+        map("n", "<leader>fr", fzf.registers, { desc = "Registers" })
+        map("n", "<leader>fk", fzf.keymaps, { desc = "Keymaps" })
+        map("n", "<leader>fc", fzf.commands, { desc = "Commands" })
+        map("n", "<leader>fs", fzf.lsp_document_symbols, { desc = "LSP Symbols" })
+    end,
+}
