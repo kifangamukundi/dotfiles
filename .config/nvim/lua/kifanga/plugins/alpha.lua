2e953dcdd0d12fa2639ff97fd76345a00361e138
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-08-13 11:25:19 -0400

    alpha-nvim: added the nvim plug for homescreen for nvim editor

---
+++ .config/nvim/lua/kifanga/plugins/alpha.lua
@@ -0,0 +1,23 @@
+return {
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		dashboard.section.header.val = { "Welcome, Kifanga Mukundi" }
+
+		dashboard.section.buttons.val = {
+			dashboard.button("<leader>sr", "Restore Session", ":SessionRestore <CR>"),
+		}
+
+		alpha.setup(dashboard.opts)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "alpha",
+			callback = function()
+				vim.opt_local.foldenable = false
+			end,
+		})
+	end,
+}
