ae115e7698ed4a1fbf733ed4cca1125ba993d8e9
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-08-18 03:36:31 -0400

    colorscheme: after moving colors out of autocomand now diffs work with my color my pencils inspired by the primeagen

--- .config/nvim/lua/kifanga/plugins/colorscheme.lua
+++ .config/nvim/lua/kifanga/plugins/colorscheme.lua
@@ -1,3 +1,21 @@
+function ColorMyPencils(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+	vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#51B3EC", bold = true })
+	vim.api.nvim_set_hl(0, "LineNr", { fg = "white", bold = true })
+	vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#FB508F", bold = true })
+
+	vim.api.nvim_set_hl(0, "CursorLine", { bg = "#212121" })
+	vim.api.nvim_set_hl(0, "Visual", { bg = "#3c3c3c" })
+
+	vim.api.nvim_set_hl(0, "Search", { bg = "#3c3c3c", fg = "#ffffff", bold = true })
+	vim.api.nvim_set_hl(0, "IncSearch", { bg = "#5f5faf", fg = "#ffffff", bold = true })
+	vim.api.nvim_set_hl(0, "MatchParen", { bg = "#444444", fg = "#ffffff", bold = true })
+end
+
 return {
 	"rose-pine/neovim",
 	name = "rose-pine",
@@ -12,5 +30,6 @@ return {
 			disable_background = true,
 		})
 		vim.cmd("colorscheme rose-pine")
+		ColorMyPencils()
 	end,
 }
