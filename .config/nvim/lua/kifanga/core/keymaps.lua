55238854d709a62acef6502787c5807e77d5ae04
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-08-18 03:26:07 -0400

    keymaps: testing with control keys for window management.

--- .config/nvim/lua/kifanga/core/keymaps.lua
+++ .config/nvim/lua/kifanga/core/keymaps.lua
@@ -6,73 +6,73 @@ local modes = { "n", "i", "v", "s", "c", "t" }
 local arrows = { "<Up>", "<Down>", "<Left>", "<Right>" }
 
 for _, mode in ipairs(modes) do
-    for _, arrow in ipairs(arrows) do
-        vim.keymap.set(mode, arrow, "<Nop>", { noremap = true })
-    end
+	for _, arrow in ipairs(arrows) do
+		vim.keymap.set(mode, arrow, "<Nop>", { noremap = true })
+	end
 end
 
 -- The "+ register" is the vim equivalent to a CTRL+C in another program, while the "* register" is the "mouse selection and middle click" equivalent (in a Gnu/Linux OS).
 -- Function to clear Vim registers, excluding the system clipboard
 function ClearRegisters()
-    local registers = {
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        "f",
-        "g",
-        "h",
-        "i",
-        "j",
-        "k",
-        "l",
-        "m",
-        "n",
-        "o",
-        "p",
-        "q",
-        "r",
-        "s",
-        "t",
-        "u",
-        "v",
-        "w",
-        "x",
-        "y",
-        "z",
-        "0",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "-",
-    }
-    for _, reg in ipairs(registers) do
-        vim.fn.setreg(reg, "")
-    end
+	local registers = {
+		"a",
+		"b",
+		"c",
+		"d",
+		"e",
+		"f",
+		"g",
+		"h",
+		"i",
+		"j",
+		"k",
+		"l",
+		"m",
+		"n",
+		"o",
+		"p",
+		"q",
+		"r",
+		"s",
+		"t",
+		"u",
+		"v",
+		"w",
+		"x",
+		"y",
+		"z",
+		"0",
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"-",
+	}
+	for _, reg in ipairs(registers) do
+		vim.fn.setreg(reg, "")
+	end
 end
 
 -- Autocmd clean up group on startup
 local cleanup_group = vim.api.nvim_create_augroup("clean-up-group", { clear = true })
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
-    group = cleanup_group,
-    command = "clearjumps",
-    desc = "Automatically clear jump list on VimEnter",
+	group = cleanup_group,
+	command = "clearjumps",
+	desc = "Automatically clear jump list on VimEnter",
 })
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
-    group = cleanup_group,
-    callback = function()
-        ClearRegisters()
-    end,
-    desc = "Automatically clear registers on VimEnter",
+	group = cleanup_group,
+	callback = function()
+		ClearRegisters()
+	end,
+	desc = "Automatically clear registers on VimEnter",
 })
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww fsession<CR>", { desc = "Tmux open project" })
@@ -105,12 +105,11 @@ vim.keymap.set("n", "<leader>x", [["_x]], { desc = "Cut without affecting yank" 
 vim.keymap.set("v", "<leader>d", [["_d]], { desc = "Delete without affecting yank" })
 vim.keymap.set("v", "<leader>c", [["_c]], { desc = "Change without affecting yank" })
 
-vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" })
-vim.keymap.set("n", "<leader>ws", "<C-w>s", { desc = "Split window horizontally" })
-vim.keymap.set("n", "<leader>we", "<C-w>=", { desc = "Make splits equal size" })
-vim.keymap.set("n", "<leader>wx", "<C-w>c", { desc = "Close current split" })
-vim.keymap.set("n", "<leader>wo", "<C-w>o", { desc = "Close other splits" })
-vim.keymap.set("n", "<leader>wr", "<C-w>r", { desc = "Rotate windows" })
+vim.keymap.set("n", "<C-a>", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<C-s>", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<C-e>", "<C-w>=", { desc = "Make splits equal size" })
+vim.keymap.set("n", "<C-c>", "<C-w>c", { desc = "Close current split" })
+vim.keymap.set("n", "<C-x>", "<C-w>o", { desc = "Close other splits" })
 
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to below split" })
@@ -127,38 +126,38 @@ vim.keymap.set("n", "<leader>bx", "<cmd>bufdo bd | enew<CR>", { desc = "Close al
 -- that apply to your entire Neovim session.
 --For the following references, workspace symbols
 vim.keymap.set("n", "<leader>qn", function()
-    local qflist = vim.fn.getqflist()
-    if #qflist == 0 then
-        vim.notify("Quickfix List is empty", vim.log.levels.INFO, { title = "Quickfix List" })
-        return
-    end
-    if not pcall(vim.cmd.cnext) then
-        pcall(vim.cmd.cfirst)
-    end
+	local qflist = vim.fn.getqflist()
+	if #qflist == 0 then
+		vim.notify("Quickfix List is empty", vim.log.levels.INFO, { title = "Quickfix List" })
+		return
+	end
+	if not pcall(vim.cmd.cnext) then
+		pcall(vim.cmd.cfirst)
+	end
 end, { desc = "Next quickfix item (wrap)" })
 
 vim.keymap.set("n", "<leader>qp", function()
-    local qflist = vim.fn.getqflist()
-    if #qflist == 0 then
-        vim.notify("Quickfix List is empty", vim.log.levels.INFO, { title = "Quickfix List" })
-        return
-    end
-    if not pcall(vim.cmd.cprevious) then
-        pcall(vim.cmd.clast)
-    end
+	local qflist = vim.fn.getqflist()
+	if #qflist == 0 then
+		vim.notify("Quickfix List is empty", vim.log.levels.INFO, { title = "Quickfix List" })
+		return
+	end
+	if not pcall(vim.cmd.cprevious) then
+		pcall(vim.cmd.clast)
+	end
 end, { desc = "Previous quickfix item (wrap)" })
 
 vim.keymap.set("n", "<leader>qx", "<Cmd>cclose<CR>", { desc = "Close quickfix list" })
 vim.keymap.set("n", "<leader>qo", "<Cmd>copen<CR>", { desc = "Open quickfix list" })
 
 vim.keymap.set("n", "<leader>qc", function()
-    local qflist = vim.fn.getqflist()
-    if #qflist == 0 then
-        vim.notify("Quickfix List is empty", vim.log.levels.INFO, { title = "Quickfix List" })
-    else
-        vim.fn.setqflist({})
-        vim.notify("Quickfix cleared", vim.log.levels.INFO, { title = "Quickfix List" })
-    end
+	local qflist = vim.fn.getqflist()
+	if #qflist == 0 then
+		vim.notify("Quickfix List is empty", vim.log.levels.INFO, { title = "Quickfix List" })
+	else
+		vim.fn.setqflist({})
+		vim.notify("Quickfix cleared", vim.log.levels.INFO, { title = "Quickfix List" })
+	end
 end, { desc = "Clear quickfix list" })
 
 -- The Location list is similar to the Quickfix list
@@ -166,38 +165,38 @@ end, { desc = "Clear quickfix list" })
 -- you're looking at right now
 -- For the following diagnostics, document symbols
 vim.keymap.set("n", "<leader>ln", function()
-    local loclist = vim.fn.getloclist(0)
-    if #loclist == 0 then
-        vim.notify("Location List is empty", vim.log.levels.INFO, { title = "Location List" })
-        return
-    end
-    if not pcall(vim.cmd.lnext) then
-        pcall(vim.cmd.lfirst)
-    end
+	local loclist = vim.fn.getloclist(0)
+	if #loclist == 0 then
+		vim.notify("Location List is empty", vim.log.levels.INFO, { title = "Location List" })
+		return
+	end
+	if not pcall(vim.cmd.lnext) then
+		pcall(vim.cmd.lfirst)
+	end
 end, { desc = "Next location item (wrap)" })
 
 vim.keymap.set("n", "<leader>lp", function()
-    local loclist = vim.fn.getloclist(0)
-    if #loclist == 0 then
-        vim.notify("Location List is empty", vim.log.levels.INFO, { title = "Location List" })
-        return
-    end
-    if not pcall(vim.cmd.lprevious) then
-        pcall(vim.cmd.llast)
-    end
+	local loclist = vim.fn.getloclist(0)
+	if #loclist == 0 then
+		vim.notify("Location List is empty", vim.log.levels.INFO, { title = "Location List" })
+		return
+	end
+	if not pcall(vim.cmd.lprevious) then
+		pcall(vim.cmd.llast)
+	end
 end, { desc = "Previous location item (wrap)" })
 
 vim.keymap.set("n", "<leader>lx", "<Cmd>lclose<CR>", { desc = "Close location list" })
 
 vim.keymap.set("n", "<leader>lo", function()
-    vim.diagnostic.setloclist({
-        open = true,
-        severity_sort = true,
-    })
-    local loclist_items = vim.fn.getloclist(0)
-    if #loclist_items == 0 then
-        vim.notify("Location List is empty, no diagnostics found.", vim.log.levels.INFO, { title = "Diagnostics" })
-    end
+	vim.diagnostic.setloclist({
+		open = true,
+		severity_sort = true,
+	})
+	local loclist_items = vim.fn.getloclist(0)
+	if #loclist_items == 0 then
+		vim.notify("Location List is empty, no diagnostics found.", vim.log.levels.INFO, { title = "Diagnostics" })
+	end
 end, { desc = "Open Location List (Diagnostics)" })
 
 -- Use = sign with motions to format according to rules
@@ -208,55 +207,55 @@ end, { desc = "Open Location List (Diagnostics)" })
 
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-        vim.hl.on_yank()
-    end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
 -- Yank into a NAMED register (general purpose): <leader>r{lowercase_register_name}y{motion/text_object}
 for i = 97, 122 do -- ASCII values for 'a' through 'z'
-    local char = string.char(i)
-    vim.keymap.set(
-        { "n", "v" },
-        "<leader>r" .. char .. "y",
-        '"' .. char .. "y",
-        { desc = "Yank to register '" .. char .. "'" }
-    )
+	local char = string.char(i)
+	vim.keymap.set(
+		{ "n", "v" },
+		"<leader>r" .. char .. "y",
+		'"' .. char .. "y",
+		{ desc = "Yank to register '" .. char .. "'" }
+	)
 end
 
 -- Append to a NAMED register: <leader>r{uppercase_register_name}y{motion/text_object}
 for i = 65, 90 do -- ASCII values for 'A' through 'Z'
-    local char = string.char(i)
-    vim.keymap.set(
-        { "n", "v" },
-        "<leader>r" .. char .. "y",
-        '"' .. char .. "y",
-        { desc = "Append yank to register '" .. string.lower(char) .. "'" }
-    )
+	local char = string.char(i)
+	vim.keymap.set(
+		{ "n", "v" },
+		"<leader>r" .. char .. "y",
+		'"' .. char .. "y",
+		{ desc = "Append yank to register '" .. string.lower(char) .. "'" }
+	)
 end
 
 -- Delete into a NAMED register: <leader>r{lowercase_register_name}d{motion/text_object}
 for i = 97, 122 do -- ASCII values for 'a' through 'z'
-    local char = string.char(i)
-    vim.keymap.set(
-        { "n", "v" },
-        "<leader>r" .. char .. "d",
-        '"' .. char .. "d",
-        { desc = "Delete to register '" .. char .. "'" }
-    )
+	local char = string.char(i)
+	vim.keymap.set(
+		{ "n", "v" },
+		"<leader>r" .. char .. "d",
+		'"' .. char .. "d",
+		{ desc = "Delete to register '" .. char .. "'" }
+	)
 end
 
 -- Delete to a NAMED register and APPEND: <leader>r{uppercase_register_name}d{motion/text_object}
 for i = 65, 90 do -- ASCII values for 'A' through 'Z'
-    local char = string.char(i)
-    vim.keymap.set(
-        { "n", "v" },
-        "<leader>r" .. char .. "d",
-        '"' .. char .. "d",
-        { desc = "Append delete to register '" .. string.lower(char) .. "'" }
-    )
+	local char = string.char(i)
+	vim.keymap.set(
+		{ "n", "v" },
+		"<leader>r" .. char .. "d",
+		'"' .. char .. "d",
+		{ desc = "Append delete to register '" .. string.lower(char) .. "'" }
+	)
 end
 
 -- Paste from any register: <leader>r{register_name}p (paste after)
@@ -264,64 +263,64 @@ end
 -- This includes named, numbered, system, and special registers.
 -- Note: 'A' or 'B' are uppercase, but when pasting, it refers to the content of 'a' or 'b'.
 local all_paste_registers = {
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "-", -- small delete register
-    "*", -- primary selection
-    "+", -- system clipboard
-    ".", -- last inserted text
-    "%", -- current file name
-    "#", -- alternate file name
-    ":", -- last command-line command
-    "_", -- black hole (though pasting from it is usually pointless, included for completeness)
-    -- No need to map " as it's the default paste
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z",
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"-", -- small delete register
+	"*", -- primary selection
+	"+", -- system clipboard
+	".", -- last inserted text
+	"%", -- current file name
+	"#", -- alternate file name
+	":", -- last command-line command
+	"_", -- black hole (though pasting from it is usually pointless, included for completeness)
+	-- No need to map " as it's the default paste
 }
 
 for _, reg in ipairs(all_paste_registers) do
-    vim.keymap.set(
-        { "n", "v" },
-        "<leader>r" .. reg .. "p",
-        '"' .. reg .. "p",
-        { desc = "Paste from register '" .. reg .. "'" }
-    )
-    vim.keymap.set(
-        { "n", "v" },
-        "<leader>r" .. reg .. "P",
-        '"' .. reg .. "P",
-        { desc = "Paste from register '" .. reg .. "' (before)" }
-    )
+	vim.keymap.set(
+		{ "n", "v" },
+		"<leader>r" .. reg .. "p",
+		'"' .. reg .. "p",
+		{ desc = "Paste from register '" .. reg .. "'" }
+	)
+	vim.keymap.set(
+		{ "n", "v" },
+		"<leader>r" .. reg .. "P",
+		'"' .. reg .. "P",
+		{ desc = "Paste from register '" .. reg .. "' (before)" }
+	)
 end
