a9619392cc3c25251c9239e444523749a5083677
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-08-21 02:18:54 -0400

    format-lint: removed formating on save from nonels

--- .config/nvim/lua/kifanga/plugins/format-lint.lua
+++ .config/nvim/lua/kifanga/plugins/format-lint.lua
@@ -1,61 +1,44 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		local builtins = null_ls.builtins
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        local builtins = null_ls.builtins
 
-		null_ls.setup({
-			sources = {
-				-- Formatting sources
-				builtins.formatting.prettierd.with({
-					filetypes = {
-						"javascript",
-						"typescript",
-						"javascriptreact",
-						"typescriptreact",
-						"css",
-						"html",
-						"json",
-						"yaml",
-						"markdown",
-						"svelte",
-					},
-				}),
-				builtins.formatting.stylua,
-				builtins.formatting.goimports,
+        null_ls.setup({
+            sources = {
+                -- Formatting sources
+                builtins.formatting.prettierd.with({
+                    filetypes = {
+                        "javascript",
+                        "typescript",
+                        "javascriptreact",
+                        "typescriptreact",
+                        "css",
+                        "html",
+                        "json",
+                        "yaml",
+                        "markdown",
+                        "svelte",
+                    },
+                }),
+                builtins.formatting.stylua,
+                builtins.formatting.goimports,
 
-				-- Linting sources
-				require("none-ls.diagnostics.eslint_d").with({
-					filetypes = {
-						"javascript",
-						"typescript",
-						"javascriptreact",
-						"typescriptreact",
-					},
-					prefer_local = "node_modules/.bin",
-				}),
-			},
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ async = true })
-						end,
-					})
-				end
-			end,
-		})
-
-		vim.keymap.set({ "n" }, "<leader>F", function()
-			vim.lsp.buf.format({ async = true })
-		end, { desc = "Format file or range (using null-ls)" })
-	end,
+                -- Linting sources
+                require("none-ls.diagnostics.eslint_d").with({
+                    filetypes = {
+                        "javascript",
+                        "typescript",
+                        "javascriptreact",
+                        "typescriptreact",
+                    },
+                    prefer_local = "node_modules/.bin",
+                }),
+            },
+        })
+    end,
 }
