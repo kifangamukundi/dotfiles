46b3640a6b25678f139a5b4c158566ce237d6573
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-08-22 02:47:02 -0400

    marks: updated to follow new keymaps designs

--- .config/nvim/lua/kifanga/plugins/mark.lua
+++ .config/nvim/lua/kifanga/plugins/mark.lua
@@ -13,7 +13,7 @@ return {
         map("n", "<leader>m", marks.toggle, { desc = "Marks: Toggle on current line" })
         -- map("n", "<leader>mn", marks.next, { desc = "Marks: Jump to next mark" })
         -- map("n", "<leader>mp", marks.prev, { desc = "Marks: Jump to previous mark" })
-        -- map("n", "<leader>mc", marks.delete_buf, { desc = "Marks: Delete all marks in buffer" })
+        map("n", "<leader>vm", marks.delete_buf, { desc = "Marks: Delete all marks in buffer" })
         -- map("n", "<leader>mo", "<cmd>MarksListBuf<CR>", { desc = "Marks: List all marks in buffer" })
         -- map("n", "<leader>mO", "<cmd>MarksListGlobal<CR>", { desc = "Marks: List all global marks" })
         -- map("n", "<leader>mC", "<cmd>delmarks A-Z<CR>", { desc = "Marks: Delete all global marks" })
