ad0fbb07bc96418b611d8baafb4a5020497d9571
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-08-23 05:41:19 -0400

    wezterm: updated the opacity

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,27 +5,15 @@ local config = wezterm.config_builder()
 config.default_prog = { "/bin/bash" }
 local act = wezterm.action
 
-config.keys = {
-	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+config.keys = { { key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") }, { key = "v", mods = "CTRL", action = act.PasteFrom("PrimarySelection") }, }
 
-	{ key = "v", mods = "CTRL", action = act.PasteFrom("PrimarySelection") },
-}
+config.mouse_bindings = { { event = { Up = { streak = 1, button = "Right" } }, mods = "NONE", action = act.PasteFrom("PrimarySelection"), }, }
 
-config.mouse_bindings = {
-	{
-		event = { Up = { streak = 1, button = "Right" } },
-		mods = "NONE",
-		action = act.PasteFrom("PrimarySelection"),
-	},
-}
-
-config.window_background_opacity = 1.0
+config.window_background_opacity = 0.98
 
 config.color_scheme = "Dracula+"
 
-config.font = wezterm.font_with_fallback({
-	{ family = "JetBrains Mono", weight = "Regular" },
-})
+config.font = wezterm.font_with_fallback({ { family = "JetBrains Mono", weight = "Regular" }, })
 
 config.font_size = 12
 config.enable_tab_bar = false
