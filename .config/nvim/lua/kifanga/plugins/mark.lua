6f0dbf4e11b1e18a7ee330d8a0b20abc13bc4397
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-08-25 15:54:40 -0400

    marks: improved the keymaps layout

--- .config/nvim/lua/kifanga/plugins/mark.lua
+++ .config/nvim/lua/kifanga/plugins/mark.lua
@@ -3,19 +3,14 @@ return {
     event = "VimEnter",
     config = function()
         local marks = require("marks")
-        local map = vim.keymap.set
 
         marks.setup({
             default_mappings = false,
             signs = true,
         })
 
-        map("n", "<leader>m", marks.toggle, { desc = "Marks: Toggle on current line" })
-        -- map("n", "<leader>mn", marks.next, { desc = "Marks: Jump to next mark" })
-        -- map("n", "<leader>mp", marks.prev, { desc = "Marks: Jump to previous mark" })
-        map("n", "<leader>vm", marks.delete_buf, { desc = "Marks: Delete all marks in buffer" })
-        -- map("n", "<leader>mo", "<cmd>MarksListBuf<CR>", { desc = "Marks: List all marks in buffer" })
-        -- map("n", "<leader>mO", "<cmd>MarksListGlobal<CR>", { desc = "Marks: List all global marks" })
-        -- map("n", "<leader>mC", "<cmd>delmarks A-Z<CR>", { desc = "Marks: Delete all global marks" })
+        vim.keymap.set("n", "<leader>m", marks.toggle, { desc = "Marks: Toggle on current line" })
+        vim.keymap.set("n", "<leader>vm", marks.delete_buf, { desc = "Marks: Delete all marks in buffer" })
+        vim.keymap.set("n", "<leader>vM", "<cmd>delmarks A-Z<CR>", { desc = "Marks: Delete all global marks" })
     end,
 }
