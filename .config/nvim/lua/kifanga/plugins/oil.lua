7121d8127f0a552f092df3a353ab7dea42eb1516
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-08-25 18:49:14 -0400

    oil: moved to the standard keymaps strategy

--- .config/nvim/lua/kifanga/plugins/oil.lua
+++ .config/nvim/lua/kifanga/plugins/oil.lua
@@ -1,31 +1,29 @@
 return {
-	"stevearc/oil.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local oil = require("oil")
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local oil = require("oil")
 
-		oil.setup({
-			use_default_keymaps = false,
-			columns = {
-				-- "icon",
-			},
-			keymaps = {
-				["<CR>"] = "actions.select",
-				["."] = "actions.toggle_hidden",
-				["-"] = "actions.parent",
-				["q"] = "actions.close",
-				["<Esc>"] = "actions.close",
-			},
-			view_options = {
-				show_hidden = true,
-			},
-		})
+        oil.setup({
+            use_default_keymaps = false,
+            columns = {
+                -- "icon",
+            },
+            keymaps = {
+                ["<CR>"] = "actions.select",
+                ["."] = "actions.toggle_hidden",
+                ["-"] = "actions.parent",
+                ["q"] = "actions.close",
+                ["<Esc>"] = "actions.close",
+            },
+            view_options = {
+                show_hidden = true,
+            },
+        })
 
-		local map = vim.keymap.set
-
-		map("n", "<leader>e", oil.open, { desc = "Open Oil in current directory" })
-		map("n", "<leader>E", function()
-			oil.open(".")
-		end, { desc = "Open Oil in current working directory" })
-	end,
+        vim.keymap.set("n", "<leader>e", oil.open, { desc = "Open Oil in current directory" })
+        vim.keymap.set("n", "<leader>E", function()
+            oil.open(".")
+        end, { desc = "Open Oil in current working directory" })
+    end,
 }
