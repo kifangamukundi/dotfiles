46d95f53f376f527faf5cedd8a8ee350ddba8f45
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-08-28 17:59:44 -0400

    marks: delection marks changed

--- .config/nvim/lua/kifanga/plugins/mark.lua
+++ .config/nvim/lua/kifanga/plugins/mark.lua
@@ -10,7 +10,7 @@ return {
         })
 
         vim.keymap.set("n", "<leader>m", marks.toggle, { desc = "Marks: Toggle on current line" })
-        vim.keymap.set("n", "<leader>vm", marks.delete_buf, { desc = "Marks: Delete all marks in buffer" })
-        vim.keymap.set("n", "<leader>vM", "<cmd>delmarks A-Z<CR>", { desc = "Marks: Delete all global marks" })
+        vim.keymap.set("n", "<leader>vx", marks.delete_buf, { desc = "Marks: Delete all marks in buffer" })
+        vim.keymap.set("n", "<leader>vX", "<cmd>delmarks A-Z<CR>", { desc = "Marks: Delete all global marks" })
     end,
 }
