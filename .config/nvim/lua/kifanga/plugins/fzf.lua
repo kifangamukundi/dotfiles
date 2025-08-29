9f9078c5cf57ee9ea94bd93cdf57866c30172362
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-08-29 08:17:12 -0400

    fzf-lua: power remaps for smooth navigation and manipulation

--- .config/nvim/lua/kifanga/plugins/fzf.lua
+++ .config/nvim/lua/kifanga/plugins/fzf.lua
@@ -47,16 +47,17 @@ return {
 
         vim.keymap.set("n", "<leader>f", fzf.files, { desc = "Find Files" })
         vim.keymap.set("n", "<C-p>", fzf.git_files, { desc = "Find Git Files" })
-        vim.keymap.set("n", "<leader>k", fzf.grep, { desc = "Find Grep" })
-        vim.keymap.set("n", "<leader>vw", fzf.grep_cword, { desc = "Search for word under cursor" })
-        vim.keymap.set("n", "<leader>vW", fzf.grep_cWORD, { desc = "Search for WORD under cursor" })
+        vim.keymap.set("n", "<C-g>", fzf.grep, { desc = "Find Grep" })
+        vim.keymap.set("n", "<leader>w", fzf.grep_cword, { desc = "Search for word under cursor" })
+        vim.keymap.set("n", "<leader>W", fzf.grep_cWORD, { desc = "Search for WORD under cursor" })
+        vim.keymap.set("v", "<leader>v", fzf.grep_visual, { desc = "Grep visual" })
+        vim.keymap.set("n", "<leader>h", fzf.help_tags, { desc = "Help Tags" })
+        vim.keymap.set("n", "<leader>k", fzf.keymaps, { desc = "Keymaps" })
 
-        vim.keymap.set("n", "<leader>vh", fzf.help_tags, { desc = "Help Tags" })
         vim.keymap.set("n", "<leader>vb", fzf.buffers, { desc = "Find Buffers" })
         vim.keymap.set("n", "<leader>vm", fzf.marks, { desc = "Marks" })
         vim.keymap.set("n", "<leader>vr", fzf.registers, { desc = "Registers" })
         vim.keymap.set("n", "<leader>vj", fzf.jumps, { desc = "Jumps" })
-        vim.keymap.set("n", "<leader>vk", fzf.keymaps, { desc = "Keymaps" })
         vim.keymap.set("n", "<leader>vc", fzf.commands, { desc = "Commands" })
         vim.keymap.set("n", "<leader>vp", fzf.manpages, { desc = "Man pages" })
 
