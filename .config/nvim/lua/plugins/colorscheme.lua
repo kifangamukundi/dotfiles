e55fa418b8e4362b62b13ee3d65f0448a7971691
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-09-13 06:31:15 -0400

    improved on the colors

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -10,14 +10,17 @@ function ColorMyPencils(color)
     vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#3e8fb0", bold = true })
-    vim.api.nvim_set_hl(0, "LineNr", { fg = "#f6c177", bold = true })
-    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#eb6f92", bold = true })
-    vim.api.nvim_set_hl(0, "CursorLine", { bg = "#212121" })
-    vim.api.nvim_set_hl(0, "Visual", { bg = "#3c3c3c" })
-    vim.api.nvim_set_hl(0, "Search", { bg = "#3c3c3c", fg = "#ffffff", bold = true })
-    vim.api.nvim_set_hl(0, "IncSearch", { bg = "#5f5faf", fg = "#ffffff", bold = true })
-    vim.api.nvim_set_hl(0, "MatchParen", { bg = "#444444", fg = "#ffffff", bold = true })
+
+    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#6e6a86", bold = true })
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#6e6a86", bold = true })
+    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#6e6a86", bold = true })
+
+    vim.api.nvim_set_hl(0, "CursorLine", { bg = "#393552" })
+
+    vim.api.nvim_set_hl(0, "Visual", { bg = "#44415a" })
+    vim.api.nvim_set_hl(0, "Search", { bg = "#31748f", fg = "#e0def4", bold = true })
+    vim.api.nvim_set_hl(0, "IncSearch", { bg = "#eb6f92", fg = "#191724", bold = true })
+    vim.api.nvim_set_hl(0, "MatchParen", { bg = "#9ccfd8", fg = "#191724", bold = true })
 end
 
 vim.api.nvim_create_autocmd("VimEnter", {
--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -45,6 +45,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
             },
         })
 
+        -- Register fzf-lua as UI backend for vim.ui.select
         fzf.register_ui_select()
 
         vim.keymap.set("n", "<leader>f", fzf.files, { desc = "Find Files" })
@@ -62,11 +63,23 @@ vim.api.nvim_create_autocmd("VimEnter", {
         vim.keymap.set("n", "<leader>vc", fzf.commands, { desc = "Commands" })
         vim.keymap.set("n", "<leader>vp", fzf.manpages, { desc = "Man pages" })
 
+        -- Default: unknown but like grd
         vim.keymap.set("n", "grd", fzf.diagnostics_document, { desc = "Document Diagnostics" })
+
+        -- Default: unknown but like grw
         vim.keymap.set("n", "grw", fzf.diagnostics_workspace, { desc = "Workspace Diagnostics" })
+
+        -- Default: gra
         vim.keymap.set("n", "gra", fzf.lsp_code_actions, { desc = "LSP Code Actions" })
+
+        -- Default: gO
+        -- Default: gD
         vim.keymap.set("n", "gD", fzf.lsp_document_symbols, { desc = "LSP Document Symbols" })
+
+        -- Default: unknown but like gW
         vim.keymap.set("n", "gW", fzf.lsp_workspace_symbols, { desc = "LSP Workspace Symbols" })
+
+        -- Default: grr
         vim.keymap.set("n", "grr", fzf.lsp_references, { desc = "LSP References" })
     end,
 })
