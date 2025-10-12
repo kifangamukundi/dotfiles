baebf8490112c72d27fd1895ba6598487ee08237
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-10-12 06:19:47 -0400

    rsync: added the rsync tool for copying files locally and on ssh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-package_list="i3 xorg xinit picom feh polybar mpd mpc mpv socat alacritty ansible pipewire pipewire-pulse wireplumber pavucontrol git"
+package_list="i3 xorg xinit picom feh polybar mpd mpc mpv socat alacritty ansible pipewire pipewire-pulse wireplumber pavucontrol git rsync"
 
 sudo apt install -y $package_list
 
