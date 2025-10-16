e7462debbf53ac25b98337258a09128b994a891e
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-10-16 17:03:27 -0400

    scripts: changed from latest go to a specific version because of problems

--- scripts/go.sh
+++ scripts/go.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
-
 set -e
 
-LATEST_VERSION=$(curl -s https://go.dev/VERSION?m=text | head -n1)
-if [ -z "$LATEST_VERSION" ]; then
-    echo "Error fetching latest Go version. Exiting."
-    exit 1
-fi
-LATEST_VERSION=${LATEST_VERSION#go}
-
+GO_VERSION="1.25.0"
 ARCH=$(uname -m)
+
 case "$ARCH" in
     x86_64) ARCH="amd64" ;;
     arm64 | aarch64) ARCH="arm64" ;;
     *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
 esac
 
-GO_FILENAME="go${LATEST_VERSION}.linux-${ARCH}.tar.gz"
+GO_FILENAME="go${GO_VERSION}.linux-${ARCH}.tar.gz"
 GO_DOWNLOAD_URL="https://go.dev/dl/${GO_FILENAME}"
 
+echo "Downloading Go ${GO_VERSION} for ${ARCH}..."
 curl -L -o "${GO_FILENAME}" "${GO_DOWNLOAD_URL}"
-if [ $? -ne 0 ]; then
-    echo "Error downloading Go. Exiting."
-    exit 1
-fi
 
 sudo rm -rf /usr/local/go
-
 sudo tar -C /usr/local -xzf "${GO_FILENAME}"
-
 rm "${GO_FILENAME}"
 
-echo "Go installation complete!"
+echo "✅ Go ${GO_VERSION} installation complete!"
+go version
