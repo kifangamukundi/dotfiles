32a2f0aad5a5906675d8a66bba855ab97afcf147
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-10-20 05:43:36 -0400

    fonts: changed and improved

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-package_list="i3 xorg xinit picom feh polybar mpd mpc mpv socat alacritty ansible pipewire pipewire-pulse wireplumber pavucontrol git rsync"
+package_list="i3 xorg xinit picom feh polybar mpd mpc mpv socat alacritty ansible pipewire pipewire-pulse wireplumber pavucontrol git rsync fonts-noto-color-emoji"
 
 sudo apt install -y $package_list
 
