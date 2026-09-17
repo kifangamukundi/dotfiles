c1405afb240aba36006990d01918e871e13362fb
kifangamukundi <pkmymcmbnhs@gmail.com>
2026-09-17 16:23:34 +0300

    fix: make node.sh use dynamic NVM_DIR variable so it works universally

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Explicitly tell the installer where we want it (so it matches your .zshrc)
+export NVM_DIR="$HOME/.config/nvm"
+
 # Download and install nvm:
-# Then nvim install version
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 # in lieu of restarting the shell
-\. "$HOME/.nvm/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
